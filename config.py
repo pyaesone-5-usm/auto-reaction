eb7fb6fc67343173cfb72fa24d7dc8f77ffdc9a5
maythushar-music <maythusharofficial@gmail.com>
2025-02-16 08:30:35 +0630

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = ""
+# -------------------------------------------------------------
+API_HASH = ""
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", "1318826936"))
+SUPPORT_GRP = "LoveisgoneForme_2"
+UPDATE_CHNL = "sasukevipmusicbotsupport"
+OWNER_USERNAME = "iwillgoforwardalone"
+    

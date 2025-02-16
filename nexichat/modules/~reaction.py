a805a1f56bcf9e2ecf31f03c7c22846dd4af00f2
pyaesone-5-usm <pyaesone5usm@thepiratebay.cloud>
2025-02-16 10:07:11 +0630

    Update ~reaction.py

--- nexichat/modules/~reaction.py
+++ nexichat/modules/~reaction.py
@@ -1,10 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import random
+from nexichat import nexichat
 
 EMOJIS = ["🥰", "❤️", "😁", "💋", "😱", "🤣", "😘", "❤️‍🔥", "👌", "🫡", "😍"]
 
-@Client.on_message(filters.incoming)
+@nexichat.on_message(filters.incoming)
 async def react_to_messages(client: Client, message: Message):
     try:
         random_emoji = random.choice(EMOJIS)

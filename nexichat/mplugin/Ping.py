d61bd4fedba0c3c79afbefcdb3cff5e90ed0a569
maythushar-music <maythusharofficial@gmail.com>
2025-02-16 08:38:04 +0630

    Add files via upload

---
+++ nexichat/mplugin/Ping.py
@@ -0,0 +1,85 @@
+import asyncio
+import logging
+import random
+import time
+import psutil
+import config
+import os
+from nexichat import _boot_
+from nexichat import get_readable_time
+from nexichat.mplugin.helpers import is_owner
+from nexichat import nexichat
+from datetime import datetime
+from pymongo import MongoClient
+from pyrogram.enums import ChatType
+from pyrogram import Client, filters
+from nexichat import db
+from config import OWNER_ID, MONGO_URL, OWNER_USERNAME
+from pyrogram.errors import FloodWait, ChatAdminRequired
+from nexichat.database.chats import get_served_chats, add_served_chat
+from nexichat.database.users import get_served_users, add_served_user
+from nexichat.database.clonestats import get_served_cchats, get_served_cusers, add_served_cuser, add_served_cchat
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
+from nexichat.mplugin.helpers import (
+    START,
+    START_BOT,
+    PNG_BTN,
+    CLOSE_BTN,
+    HELP_BTN,
+    HELP_BUTN,
+    HELP_READ,
+    HELP_START,
+    SOURCE_READ,
+)
+
+IMG = [
+    "https://graph.org/file/210751796ff48991b86a3.jpg",
+    "https://graph.org/file/7b4924be4179f70abcf33.jpg",
+    "https://graph.org/file/f6d8e64246bddc26b4f66.jpg",
+    "https://graph.org/file/63d3ec1ca2c965d6ef210.jpg",
+    "https://graph.org/file/9f12dc2a668d40875deb5.jpg",
+    "https://graph.org/file/0f89cd8d55fd9bb5130e1.jpg",
+    "https://graph.org/file/e5eb7673737ada9679b47.jpg",
+    "https://graph.org/file/2e4dfe1fa5185c7ff1bfd.jpg",
+    "https://graph.org/file/36af423228372b8899f20.jpg",
+    "https://graph.org/file/c698fa9b221772c2a4f3a.jpg",
+    "https://graph.org/file/61b08f41855afd9bed0ab.jpg",
+    "https://graph.org/file/744b1a83aac76cb3779eb.jpg",
+    "https://graph.org/file/814cd9a25dd78480d0ce1.jpg",
+    "https://graph.org/file/e8b472bcfa6680f6c6a5d.jpg",
+]
+
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - _boot_)
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    UP = f"{get_readable_time((bot_uptime))}"
+    CPU = f"{cpu}%"
+    RAM = f"{mem}%"
+    DISK = f"{disk}%"
+    return UP, CPU, RAM, DISK
+
+
+@Client.on_message(filters.command("ping"))
+async def ping(client: Client, message: Message):
+    bot_id = client.me.id
+    start = datetime.now()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    loda = await message.reply_photo(
+        photo=random.choice(IMG),
+        caption="ᴘɪɴɢɪɴɢ...",
+    )
+
+    ms = (datetime.now() - start).microseconds / 1000
+    await loda.edit_text(
+        text=f"нey вαву!!\n{(await client.get_me()).mention} ᴄʜᴀᴛʙᴏᴛ ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n\n**➥** `{ms}` ms\n**➲ ᴄᴘᴜ:** {CPU}\n**➲ ʀᴀᴍ:** {RAM}\n**➲ ᴅɪsᴋ:** {DISK}\n**➲ ᴜᴘᴛɪᴍᴇ »** {UP}\n\n<b>||**๏ мαdє ωιтн ❣️ ву [ᴡᴀɴɢ ʟɪɴ](https://t.me/{OWNER_USERNAME}) **||</b>",
+        reply_markup=InlineKeyboardMarkup(PNG_BTN),
+    )
+    if message.chat.type == ChatType.PRIVATE:
+        await add_served_cuser(bot_id, message.from_user.id)
+        await add_served_user(message.from_user.id)
+    else:
+        await add_served_cchat(bot_id, message.chat.id)
+        await add_served_chat(message.chat.id)

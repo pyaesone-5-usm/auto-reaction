c8e1cf9b4a9c7a8bfc63c516230a822aca7f4cc9
pyaesone-5-usm <pyaesone5usm@thepiratebay.cloud>
2025-02-16 09:47:42 +0630

    Update ~reaction.py

--- nexichat/modules/~reaction.py
+++ nexichat/modules/~reaction.py
@@ -2,12 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from nexichat import nexichat
 
-EMOJIS = ["🥰", "❤️", "😁", "💋", "😱", "🤣", "😘", "❤️‍🔥", "👌", "🫡", "😍"]
-
 @nexichat.on_message(filters.incoming)
 async def react_to_messages(client: Client, message: Message):
     try:
-        react = random.choice(EMOJIS)
-        await message.react()
+        await message.react("🥰", "❤️", "😁", "💋", "😱", "🤣", "😘", "❤️‍🔥", "👌", "🫡", "😍", "👍")
     except Exception as e:
         print(f"Failed to react to message: {e}")
